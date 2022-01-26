e776548dcc5e121f17092b5c039bc849953ce95f
1iryu <93420248+1iryu@users.noreply.github.com>
2022-01-26 16:09:41 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+import time
+import Reprint
+
+
+def print_processing_time(function):
+    start_time = time.time()
+    function()
+    end_time = time.time()
+    print(end_time-start_time)
+
+
+reprint = Reprint.Reprint()
+reprint2 = Reprint.ReprintVersion2()
+
+def test_ver1():
+    for i in range(100):
+        list = [i, i+1, i+2]
+        reprint.do(list)
+
+
+def test_ver2():
+    for i in range(100):
+        list = [i, i+1, i+2]
+        reprint2.do(list)
+
+
+print_processing_time(test_ver1)
+print_processing_time(test_ver2)

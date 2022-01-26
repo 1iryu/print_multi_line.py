e534b14381d0e16f9a43017cada1bbfe1bf5ceb3
1iryu <93420248+1iryu@users.noreply.github.com>
2022-01-26 16:08:34 +0900

    Update reprint.py

--- reprint.py
+++ reprint.py
@@ -3,15 +3,6 @@ import sys
 import time
 
 
-#example
-
-# reprint = Reprint()
-# for i in range(10):
-#     list = [i,i+1,i+2]
-#     reprint.do(list)
-#     time.sleep(1)
-
-
 class Reprint():
     def __init__(self):
         self.first = False
@@ -19,12 +10,10 @@ class Reprint():
         os.system("")
 
     def cursor_up(self, length: int):
-        sys.stdout.write("\033[F" * length)
+        print("\033[F" * length,end="")
 
     def cursor_down(self, length: int):
-        sys.stdout.write("\n" * length)
-
-
+        print("\n" * length,end="")
 
     def do(self, list_text: list):
         if(self.first == False):
@@ -39,5 +28,32 @@ class Reprint():
                 print(list_text[i], end="")
                 self.cursor_down(i+1)
         else:
-            print("Incorrect count of rows")
+            print("It's not the same as the number on the first list you gave you.")
 
+
+class ReprintVersion2():
+    def __init__(self):
+        self.first = False
+        self.lines_length = 0
+        os.system("")
+
+    def cursor_up(self, length):
+        print(f"\r\033[{length}A", end="")
+
+    def cursor_down(self, length):
+        print(f"\r\033[{length}B", end="")
+
+    def do(self, list_text: list):
+        if(self.first == False):
+            self.first = True
+            self.lines_length = len(list_text)
+            for text in list_text:
+                print(text)
+        if self.lines_length == len(list_text):
+            list_text.reverse()
+            for i in range(len(list_text)):
+                self.cursor_up(i+1)
+                print(list_text[i], end="")
+                self.cursor_down(i+1)
+        else:
+            print("It's not the same as the number on the first list you gave you.")

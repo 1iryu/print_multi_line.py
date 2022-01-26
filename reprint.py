cf4d3df88837c317883a98ff0a2736a3dbd97804
1iryu <93420248+1iryu@users.noreply.github.com>
2022-01-26 12:10:40 +0900

    Update reprint.py

--- reprint.py
+++ reprint.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 
 #example
@@ -7,8 +8,9 @@ import sys
 # reprint = Reprint()
 # for i in range(10):
 #     list = [i,i+1,i+2]
-#     reprint.reprint_multi_line_by_list(list)
+#     reprint.do(list)
 #     time.sleep(1)
+
 
 class Reprint():
     def __init__(self):
@@ -16,29 +18,15 @@ class Reprint():
         self.lines_length = 0
         os.system("")
 
-    def cursor_up(self,length: int):
+    def cursor_up(self, length: int):
         sys.stdout.write("\033[F" * length)
 
-    def cursor_down(self,length: int):
+    def cursor_down(self, length: int):
         sys.stdout.write("\n" * length)
 
-    def reprint_multi_line_by_text(self, text: str):
-        if(self.first == False):
-            self.first = True
-            self.lines_length = len(text.split("\n"))
-            print(text)
-        if self.lines_length == len(text.split("\n")):
-            lines  = text.split("\n")
-            lines.reverse()
-            for i in range(len(lines)):
-                self.cursor_up(i+1)
-                print(list_text[i], end="")
-                self.cursor_down(i+1)
-        else:
-            print("Incorrect count of rows")
 
 
-    def reprint_multi_line_by_list(self, list_text: list):
+    def do(self, list_text: list):
         if(self.first == False):
             self.first = True
             self.lines_length = len(list_text)
@@ -48,7 +36,8 @@ class Reprint():
             list_text.reverse()
             for i in range(len(list_text)):
                 self.cursor_up(i+1)
-                print(list_text[i],end="")
+                print(list_text[i], end="")
                 self.cursor_down(i+1)
         else:
             print("Incorrect count of rows")
+
